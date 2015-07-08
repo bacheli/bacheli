6a522381625e99b65d36b89fce4925f5cc981c09
bacheli <sercheli-25@hotmail.com>
2015-07-08 12:24:47 +0200

    1ª Conección

---
+++ index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <meta name="author" content="Script Tutorials" />
+  <meta name="description" content="Responsive Websites Using BootStrap - demo page">
+  <title>De Bacheli Para Leydi</title>
+  <!-- Bootstrap -->
+  <link href="libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="libs/css/animate.min.css" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
+
+
+
+</head>
+<body>
+<header>
+  <nav class="navbar navbar-default navbar-fixed-top " role="navigation">
+  <div class="container">
+    
+    <div class="navbar-header">
+
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#b-menu-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+      <a class="navbar-brand animated rotateInDownLeft" href="#">Bacheli</a>
+    </div>
+  
+    <div class="collapse navbar-collapse" id="b-menu-1">
+        
+        <ul class="nav navbar-nav navbar-right">
+
+        <li><a href="#">Te Kiero</a></li>
+        <li><a href="#">Te Extraño</a></li>
+        <li><a href="#">Te Amo</a></li>
+        <li><a href="#">Mi Bebita</a></li>
+        <li><a href="#">Gatita</a></li>
+
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class=""></span><b class="caret"></b></a>
+           <ul class="dropdown-menu">
+                <li><a href="#">curriculum</a></li>
+                <li><a href="#">recuerdos</a></li>
+                <li><a href="#">familia</a></li>
+
+                <li><a href="https://www.facebook.com/" target="_blank"><img alt="siguenos en facebook" height="32" src="img/face.png" title="siguenos en facebook" width="32" /></a></li>
+                <li> <a href="https://twitter.com/" target="_blank"><img alt="siguenos en Twitter" height="32" src="img/Twitter+NEW.png" title="siguenos en Twitter" width="32" /></a></li>
+
+
+              </ul>
+        </li>
+
+      </ul>
+    </div>
+    </div>
+  </nav>
+</header>
+<section style="padding-top: 50px">
+  <div class="jumbotron animated zoomIn">
+    <div class="container text-center">
+      <marquee behavior="scroll">
+          <h1> CON CARIÑO PARA MI MOCHI </h1>
+      </marquee>
+     
+    </div>
+  </div>
+</section>
+<section>
+<div class="container galeria">
+  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+  </ol>
+
+  <div class="carousel-inner">
+    <div class="item active">
+      <div class="row animated zoomIn">
+        <div class="col-sm-6">
+         <img src="img/bebe.jpg" alt="Leidy" class="img-thumbnail">
+        </div>
+        <div class="col-sm-6">
+         <img src="img/leidy.jpg" alt="Leidy" class="img-thumbnail hidden-xs">
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="row animated zoomIn">
+        <div class="col-sm-6">
+         <img src="img/leydi.jpg" alt="Leidy" class="img-thumbnail hidden-xs">
+        </div>
+        <div class="col-sm-6">
+         <img src="img/2.jpg" alt="Leidy" class="img-thumbnail">
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="row animated zoomIn ">
+        <div class="col-sm-6">
+         <img src="img/1.jpg" alt="Leidy" class="img-thumbnail hidden-xs">
+        </div>
+        <div class="col-sm-6">
+         <img src="img/preciosa.jpg" alt="Leidy" class="img-thumbnail">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div>
+</div>
+</section>
+<section>
+<div class="container ">
+<div class="row">
+  <article class="col-sm-6 col-md-4">
+    <div class="separator"></div>
+    <h2>TE KIERO</h2>
+    <p class="text-justify">
+      Gracias por existir, gracias.
+      Gracias por fijarte en mi, por dejarme disfrutar de tu presencia, por mirarme y por hablarme.
+      Gracias por dejarme soñar contigo, por hacerme vivir de ti, por provocar la ilusión que lleva tu nombre.
+      Gracias por elegirme, elegirme para acompañarte, elegirme para apoyarte, elegirme para sentir.
+      Gracias por respirar, andar, mirar, hablar, despertar, sonreír, escuchar…. gracias por existir.
+      Gracias y un millón de gracias por dejarme amarte..
+      ... para ti.. TE KIERO..........
+    </p>
+    <a href="#" title="Ver más" class="boton">Ver más</a>
+  </article>
+  
+  <article class="col-sm-6 col-md-4">
+    <div class="separator"></div>
+    <h2>TE NECESITO</h2>
+    <p class="text-justify">
+      Lo que siento cuando estoy cerca de ti es para mí lo más hermoso que me puede pasar.
+      Eres la persona más dulce, más tierna, más hermosa… eres la persona perfecta. Tú lo significas todo para mí.
+      Sólo un beso tuyo, una mirada, una caricia, podría transportarme al paraíso… No me cansaré de decirlo, te amo
+      Te necesito, para mirarte y perderme en tu mirada. Para poder soñar cada noche. Para despertar y pronunciar tu nombre.
+      Anhelo un beso tuyo, que tu calor me quite el frio…
+      Quiero que siempre sepas que vivo para amarte...
+      TE NECESITO EN MI VIDA PARA SEGUIR ♥♥♥
+    </p>
+    <a href="#" title="Ver más" class="boton">Ver más</a>
+  </article>
+  <article class="col-sm-12 col-md-4">
+    <div class="separator"></div>
+    <h2>CON AMOR ♥</h2>
+    <div class="embed-responsive embed-responsive-16by9">
+      <video class="embed-responsive-item" width="560" height="315" src="media/Leidyteamo.mp4" controls></video>
+      
+    </div>
+  </article>
+</div>
+</div>
+</section>
+<footer>
+  <div class="container">
+    <br><hr>
+    <h2 class="text-center">♥♥♥ TE KIERO ♥♥♥ AUNQUE SEAS UNA ODIOSA MI BEBE ♥♥</h2>
+    <br>
+  </div>
+</footer>
+</body>
+<script src="js/jquery.min.js"></script>
+<script src="libs/bootstrap/js/bootstrap.min.js"></script>
+</html>
